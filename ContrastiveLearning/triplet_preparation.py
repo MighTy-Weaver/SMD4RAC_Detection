6022f42448eb382d0dbfbf59d4276c6efe65aaee
MighTy-Weaver <1874240442@qq.com>
2021-12-04 01:59:50 +0800

    update

--- ContrastiveLearning/triplet_preparation.py
+++ ContrastiveLearning/triplet_preparation.py
@@ -1,4 +1,6 @@
 import argparse
+import os.path
+from itertools import permutations
 from itertools import product
 from random import sample
 
@@ -26,31 +28,44 @@ X['Date'] = data['Time'].apply(lambda x: x.split(' ')[0])
 dates = X['Date'].unique()
 rooms = normal_room_list + poor_room_list
 
-room_data_dict = {r: [] for r in rooms}
-for r in tqdm(rooms, desc="filtering data samples for ALL rooms: "):
-    for d in tqdm(dates):
-        if len(X[(X.Date == d) & (X.Location == r)]) == 48:
-            room_data_dict[r].append(d)
-np.save('../data/room_date_dict.npy', room_data_dict)
-room_data_dict_sample = {r: sample(room_data_dict[r], int(len(room_data_dict[r]) * sample_frac)) for r in rooms}
-np.save('../data/room_date_dict_sample{}.npy'.format(sample_frac), room_data_dict_sample)
-print(room_data_dict_sample)
+if os.path.exists('../data/room_date_dict.npy'):
+    room_data_dict = np.load('../data/room_date_dict.npy', allow_pickle=True).item()
+    print('Loading pre-generated room date summary dict, no sample applied.')
+    print(room_data_dict.keys(), list(room_data_dict.values())[0])
+else:
+    room_data_dict = {r: [] for r in rooms}
+    for r in tqdm(rooms, desc="filtering data samples for ALL rooms: "):
+        for d in tqdm(dates):
+            if len(X[(X.Date == d) & (X.Location == r)]) == 48:
+                room_data_dict[r].append(d)
+    np.save('../data/room_date_dict.npy', room_data_dict)
+    room_data_dict_sample = {r: sample(room_data_dict[r], int(len(room_data_dict[r]) * sample_frac)) for r in rooms}
+    np.save('../data/room_date_dict_sample{}.npy'.format(sample_frac), room_data_dict_sample)
 
+pos_df_list = []
 for n in tqdm(normal_room_list, desc="Generating triplets for NORMAL rooms: "):
     for p in tqdm(poor_room_list):
-        for n_d in tqdm(room_data_dict_sample[n]):
-            pos_neg = product([i for i in room_data_dict_sample[n] if i != n_d], room_data_dict_sample[p])
-            for pos, neg in tqdm(pos_neg):
-                triplet_csv = triplet_csv.append({'anchor': (n, n_d), 'pos': (n, pos), 'neg': (p, neg)},
-                                                 ignore_index=True)
-triplet_csv.to_csv('./triplet.csv', index=False)
+        pos_list = permutations(room_data_dict[n], 2)
+        neg_list = room_data_dict[p]
+        pos_neg_list = list(product(pos_list, neg_list))
+        n_p_df = pd.DataFrame(data={'anchor': [i[0][0] for i in pos_neg_list], 'pos': [i[0][1] for i in pos_neg_list],
+                                    'neg': [i[1] for i in pos_neg_list], 'anchor_room': [n for _ in pos_neg_list],
+                                    'pos_room': [n for _ in pos_neg_list], 'neg_room': [p for _ in pos_neg_list]})
+        pos_df_list.append(n_p_df)
+pos_triplet_csv = pd.concat(pos_df_list, ignore_index=True)
+pos_triplet_csv.to_csv('./pos_triplet.csv', index=False)
 
+neg_df_list = []
 for p in tqdm(poor_room_list, desc="Generating triplets for POOR rooms: "):
     for n in tqdm(normal_room_list):
-        for p_d in tqdm(room_data_dict_sample[p]):
-            pos_neg = product([i for i in room_data_dict_sample[p] if i != p_d], room_data_dict_sample[n])
-            for pos, neg in tqdm(pos_neg):
-                triplet_csv = triplet_csv.append({'anchor': (p, p_d), 'pos': (p, pos), 'neg': (n, neg)},
-                                                 ignore_index=True)
+        neg_list = permutations(room_data_dict[p], 2)
+        pos_list = room_data_dict[p]
+        neg_pos_list = product(neg_list, pos_list)
+        p_n_df = pd.DataFrame(data={'anchor': [i[0][0] for i in neg_pos_list], 'pos': [i[0][1] for i in neg_pos_list],
+                                    'neg': [i[1] for i in neg_pos_list], 'anchor_room': [p for _ in neg_pos_list],
+                                    'pos_room': [p for _ in neg_pos_list], 'neg_room': [n for _ in neg_pos_list]})
+        neg_df_list.append(p_n_df)
+neg_triplet_csv = pd.concat(neg_df_list, ignore_index=True)
+neg_triplet_csv.to_csv('./neg_triplet.csv', index=False)
 
-triplet_csv.to_csv('./triplet.csv', index=False)
+pd.concat([pos_triplet_csv, neg_triplet_csv], ignore_index=True).to_csv('./triplet.csv', index=False)
