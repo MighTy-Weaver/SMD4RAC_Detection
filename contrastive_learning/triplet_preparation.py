4e82a7e6a4eefac522f6f6f814d51673d4ce5dae
MighTy-Weaver <1874240442@qq.com>
2021-12-12 23:00:53 +0800

    update

--- contrastive_learning/triplet_preparation.py
+++ contrastive_learning/triplet_preparation.py
@@ -28,9 +28,11 @@ X = data.drop(['Weekday', 'Total', 'Lighting', 'Socket', 'WaterHeater', 'Time'],
 Y = data['AC']
 X['Date'] = data['Time'].apply(lambda x: x.split(' ')[0])
 dates = X['Date'].unique()
+print(dates)
 rooms = normal_room_list + poor_room_list
+print(rooms)
 
-if os.path.exists('../data/room_date_dict.npy'):
+if False:  # os.path.exists('../data/room_date_dict.npy'):
     room_data_dict = np.load('../data/room_date_dict.npy', allow_pickle=True).item()
     print('Loading pre-generated room date summary dict, no sample applied.')
     print(room_data_dict.keys(), list(room_data_dict.values())[0])
