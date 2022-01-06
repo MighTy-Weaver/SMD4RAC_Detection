ddaf54c235ae007e2244164d27e3683ac113dbd0
MighTy-Weaver <1874240442@qq.com>
2022-01-06 21:44:24 +0800

    update

--- contrastive_learning/utils.py
+++ contrastive_learning/utils.py
@@ -9,6 +9,7 @@ all_room_efficiency_list = list(efficiency_dict.values())
 normal_room_list = [i for i in all_room_list if efficiency_dict[i] >= mean(all_room_efficiency_list)]
 poor_room_list = [i for i in all_room_list if i not in normal_room_list]
 print("Efficiency list loaded, {} normal rooms and {} poor rooms".format(len(normal_room_list), len(poor_room_list)))
+print("Mean efficiency is {}".format(mean(all_room_efficiency_list)))
 
 
 def cosine_similarity_loss(x, y):
