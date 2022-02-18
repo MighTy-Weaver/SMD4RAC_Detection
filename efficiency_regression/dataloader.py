4793ef3221f8e033261a72b2acebf579a9cfef64
MighTy-Weaver <1874240442@qq.com>
2022-02-18 16:28:47 +0800

    update

--- efficiency_regression/dataloader.py
+++ efficiency_regression/dataloader.py
@@ -43,5 +43,36 @@ class AC_Normal_Dataset(Dataset):
         return len(self.room_date_list)
 
 
+class AC_Sparsed_Dataset(Dataset):
+    def __init__(self):
+        if mode not in ['trn', 'val']:
+            raise NotImplementedError("mode must be either 'trn' or 'val'")
+        self.room_date_dict = dict(np.load('../data/room_date_dict.npy', allow_pickle=True).item())
+        self.room_date_list = []
+        for room, value in self.room_date_dict.items():
+            self.room_date_list.extend([(room, d) for d in value])
+
+        self.room_date_list = shuffle(self.room_date_list, random_state=621)
+
+        if test:
+            self.room_date_list = self.room_date_list[:500]
+
+        if mode == 'trn':
+            self.room_date_list = self.room_date_list[:int(len(self.room_date_list) * trn_ratio)]
+        else:
+            self.room_date_list = self.room_date_list[int(len(self.room_date_list) * trn_ratio):]
+
+        self.data = pd.read_csv('../data/20201230_20210815_data_compiled_half_hour.csv', index_col=None)
+        self.X = self.data.drop(['Weekday', 'Total', 'Lighting', 'Socket', 'WaterHeater', 'Time'], axis=1)
+        self.X['Date'] = self.data['Time'].apply(lambda x: x.split(' ')[0])
+        print("Normal Regression DATALOADER: finished loading original data, total {}".format(len(self.room_date_list)))
+
+    def __getitem__(self, item):
+        pass
+
+    def __len__(self):
+        pass
+
+
 t = AC_Normal_Dataset()
 print(t.room_date_dict)
