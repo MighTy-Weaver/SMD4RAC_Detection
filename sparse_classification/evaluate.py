3f98f6be292a5fb8daf3661aba1dbbe5670afb23
MighTy-Weaver <1874240442@qq.com>
2022-06-01 02:13:30 +0800

    update

--- sparse_classification/evaluate.py
+++ sparse_classification/evaluate.py
@@ -4,8 +4,7 @@ import warnings
 
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn
-from sklearn.metrics import r2_score
+import pandas as pd
 from tqdm import tqdm
 
 warnings.filterwarnings('ignore')
@@ -19,89 +18,31 @@ plt.rcParams["font.family"] = "Times New Roman"
 plt.rcParams["savefig.bbox"] = "tight"
 checkpoints = glob.glob('./*checkpoint*/')
 
-gs_choices = [5, 10, 25, 50, 75, 100]
+gs_choices = [5, 10, 25, 50, 100, 150, 200, 400]
+data_num_choices = [10000, 20000, 50000, 100000, 150000, 200000, 300000, 400000]
 model_choices = ['lstm', 'bilstm', 'transformer', 'lstm-transformer', 'bilstm-transformer']
 
-model_dict = {mc: {g: None for g in gs_choices} for mc in model_choices}
-
-# def find_latest_record(files: list):
-#     if not files:
-#         return None
-#         # return {'trn_loss': [], 'val_loss': [], 'trn_r2': [], 'val_r2': []}
-#     maximum_num = max(int(f.split('.')[-2][7:11]) for f in files)
-#     for f in files:
-#         if str(maximum_num) in f:
-#             return np.load(f, allow_pickle=True).item()
-#     raise Exception(f"Maximum not found in {files}")
-
+model_dict = {}
+csv_record = pd.DataFrame(
+    columns=['model', 'gs', 'data_number', 'best_train_acc', 'best_valid_acc', 'best_train_f1', 'best_valid_f1'])
 
 for f in tqdm(checkpoints):
     info = f.split('_')
     model_version = info[0].replace("./", '')
     epoch_num = int(info[3].replace('e', ''))
     gs = int(info[6].replace('gs', ''))
-    preds = np.load(f'{f}/best_pred.npy', allow_pickle=True).tolist()
-    labels = np.load(f'{f}/best_label.npy', allow_pickle=True).tolist()
-
-    preds = preds[int(0.8 * len(preds)):]
-    labels = labels[int(0.8 * len(labels)):]
-
-    # plt.scatter(x=labels, y=preds, label="(Label, Prediction)")
-    real_range = np.linspace(min(list(preds + labels)), max(list(preds + labels)))
-    seaborn.regplot(x=labels, y=preds, label="(Label, Prediction)", scatter=True)
-    plt.plot(real_range, real_range, color='m', linestyle="-.", linewidth=1, label="Identity Line (y=x)")
-    plt.xlabel(f"Truth label\n$Accuracy Score\t{round(r2_score(labels, preds), 5)}$")
-    plt.ylabel("Prediction")
-    plt.title(f"Model: {model_version}\tGroup Size: {gs}")
-    plt.legend()
-    plt.savefig(f'./truth_pred_plot/{model_version}_{gs}.png', bbox_inches='tight')
-    plt.clf()
-
+    data_num = int(info[-1].replace('numdata', ''))
     record = np.load(f'{f}/record.npy', allow_pickle=True).item()
-    model_dict[model_version][gs] = record
-    if record:
-        length = range(1, len(record['trn_loss']) + 1)
-        plt.plot(length, record['trn_loss'], label='Training Loss')
-        plt.plot(length, record['val_loss'], label='Validation Loss')
-        plt.xlabel("Epoch")
-        plt.ylabel("Loss")
-        plt.title(f"Model {model_version} Loss Curve")
-        plt.legend()
-        plt.savefig(f'./truth_pred_plot/{model_version}_{gs}_loss.png', bbox_inches='tight')
-        plt.clf()
-
-        plt.plot(length, record['trn_acc'], label='Training Acc')
-        plt.plot(length, record['val_acc'], label='Validation Acc')
-        plt.xlabel("Epoch")
-        plt.ylabel("Acc")
-        plt.title(f"Model {model_version} Acc Curve")
-        plt.legend()
-        plt.savefig(f'./truth_pred_plot/{model_version}_{gs}_acc.png', bbox_inches='tight')
-        plt.clf()
-
-for m in model_choices:
-    g_choices = [g for g in gs_choices if model_dict[m][g]]
-    trn_loss_min = [min(model_dict[m][g]['trn_loss']) for g in gs_choices if model_dict[m][g]]
-    val_loss_min = [min(model_dict[m][g]['val_loss']) for g in gs_choices if model_dict[m][g]]
-    trn_acc_max = [max(model_dict[m][g]['trn_acc']) for g in gs_choices if model_dict[m][g]]
-    val_acc_max = [max(model_dict[m][g]['val_acc']) for g in gs_choices if model_dict[m][g]]
-
-    print(m, max(val_acc_max), g_choices[val_acc_max.index(max(val_acc_max))])
-
-    plt.plot(g_choices, trn_loss_min, label="Min Training Loss")
-    plt.plot(g_choices, val_loss_min, label="Min Validation Loss")
-    plt.xlabel("Group Size")
-    plt.ylabel("Loss")
-    plt.title(f"Model: {m} Loss - GroupSize Curve")
-    plt.legend()
-    plt.savefig(f'./model_plot/{m}_loss.png', bbox_inches='tight')
-    plt.clf()
-
-    plt.plot(g_choices, trn_acc_max, label="Max Training Accuracy")
-    plt.plot(g_choices, val_acc_max, label="Max Validation Accuracy")
-    plt.xlabel("Group Size")
-    plt.ylabel("Accuracy")
-    plt.title(f"Model: {m} Accuracy - GroupSize Curve")
-    plt.legend()
-    plt.savefig(f'./model_plot/{m}_acc.png', bbox_inches='tight')
-    plt.clf()
+    train_pred = np.load(f'{f}/best_train_pred.npy', allow_pickle=True).tolist()
+    train_label = np.load(f'{f}/best_train_label.npy', allow_pickle=True).tolist()
+    valid_pred = np.load(f'{f}/best_valid_pred.npy', allow_pickle=True).tolist()
+    valid_label = np.load(f'{f}/best_valid_label.npy', allow_pickle=True).tolist()
+    model_dict[(model_version, gs, data_num)] = [train_pred, valid_pred, train_label, valid_label]
+    if len(record['trn_acc']) < epoch_num:
+        print("\nWARNING: model: {} gs: {} data: {} hasn't fully ran".format(model_version, gs, data_num))
+    csv_record = csv_record.append(
+        {'model': model_version, 'gs': gs, 'data_number': data_num, 'best_train_acc': max(record['trn_acc']),
+         'best_valid_acc': max(record['val_acc']), 'best_train_f1': max(record['trn_f1']),
+         'best_valid_f1': max(record['val_f1'])}, ignore_index=True)
+csv_record.to_csv('../sparse_record.csv', index=False)
+np.save('../sparse_pred_label.npy', model_dict)
